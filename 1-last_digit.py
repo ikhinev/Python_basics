9a620c67fa138a6b648eeb023f732ad7acb0c209
ikhinev <81270778+ikhinev@users.noreply.github.com>
2024-11-15 21:48:23 +0100

    Update 1-last_digit.py

--- 1-last_digit.py
+++ 1-last_digit.py
@@ -1,1 +1,9 @@
-
+import random
+number = random.randint(-10000, 10000)
+x = number % 10
+if x > 5:
+	print(f'last digit of {number} is {x} and is greater than 5')
+elif x == 0:
+	print(f'last digit of {number} is {x} and is 0')
+else:
+	print(f'last digit of {number} is {x} and is less than 6 and not 0')
