b5510c17092838a371e4c763cbd5500a72acb063
ikhinev <81270778+ikhinev@users.noreply.github.com>
2024-11-20 11:20:00 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,39 @@
+def add(a, b):
+    """
+    Add two numbers.
 
+    Parameters:
+    a : The first number.
+    b : The second number.
+
+    Returns:
+    The sum of the two numbers.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Subtracts the second number from the first number.
+
+    Parameters:
+    a: The first number.
+    b: The second number.
+
+    Returns:
+    The difference between the two numbers.
+    """
+    return a - b
+
+def multiply(a, b):
+    """
+    Multiply two numbers.
+
+    Parameters:
+    a: The first number.
+    b: The second number.
+
+    Returns:
+    The product of the two numbers.
+    """
+    return a * b
+  
